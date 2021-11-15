08ade80b24230554be14e9da1c754e1b27d636ee
fucult01 <94074467+fucult01@users.noreply.github.com>
2021-11-15 12:04:35 -0300

    Create part-1.sh

---
+++ web-dev-fundamentals/project-01-Unit-&-Bash/day-03-unix-bash-part-1/part-1.sh
@@ -0,0 +1,14 @@
+mkdir unix_tests
+touch trybe.txt
+mv trybe.txt trybe_backup.txt
+mv trybe.txt trybe-old.txt
+mkdir backup
+mv trybe_backup.txt backup
+mkdir backup2
+mv backup/trybe_backup.txt backup2
+rm backup
+mv backup2 backup
+pwd
+ls
+rm -rf backup
+clear
