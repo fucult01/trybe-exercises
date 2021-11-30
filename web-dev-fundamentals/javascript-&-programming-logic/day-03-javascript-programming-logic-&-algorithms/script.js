d43d524a64281d9fc7eb7dce554a9e75be4dc291
Artur <fucult96@gmail.com>
2021-11-30 18:25:45 -0300

    something

--- web-dev-fundamentals/javascript-&-programming-logic/day-03-javascript-programming-logic-&-algorithms/script.js
+++ web-dev-fundamentals/javascript-&-programming-logic/day-03-javascript-programming-logic-&-algorithms/script.js
@@ -6,7 +6,7 @@ for(let i=0; i<y; i+=1) {
     square += asterisk;
 };
 for(let i=0; i<y; i+=1) {
-    console.log(square)
+    console.log(square);
 }
 
 // 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
@@ -15,7 +15,7 @@ let triangle = '';
 let asterisk2 = '*';
 for(let i=0; i<k; i+=1) {
     triangle += asterisk2;
-    console.log(triangle)
+    console.log(triangle);
 } 
 
 // 3 - Agora inverta o lado do triângulo. 
@@ -49,31 +49,30 @@ console.log(pyramid);
 // Bonus exercise
 // 5 - Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
 let w = 7; 
-let mid = (w+1)/2;
+let pyramidMiddle = (w+1)/2;
 let pyramidHollow = '';
-for (let q = 1; q<mid; q+=1) {
-    for(let j=1; j<=(mid-q); j+=1) {
+for (let q = 1; q<pyramidMiddle; q+=1) {
+    for(let j=1; j<=pyramidMiddle-q; j+=1) {
         pyramidHollow += ' ';
     } if(q === 1) {
-    pyramidHollow += '*';
-} else {
-    pyramidHollow += '*';
-    for(let j=1; j<= 2*q-3; j+=1) {
-        pyramidHollow += ' ';
-    }
-    pyramidHollow += '*';
-}
-pyramidHollow += '\n';
-}
-   for (q = mid; q<=w; q+=1) {
-     for(let j=1; j<=q-mid; j+=1) {
-        pyramidHollow += '';
-    }
-    if(q === w) {
         pyramidHollow += '*';
     } else {
         pyramidHollow += '*';
-        for (let j=1; j<=2*(w-q)-3; j+=1) {
+    for(let j=1; j<= 2*q-3; j+=1) {
+        pyramidHollow += ' ';
+    }
+        pyramidHollow += '*';
+  }
+    pyramidHollow += '\n';
+}
+for (q = pyramidMiddle; q<=w; q+=1) {
+    for(let j=0; j<pyramidMiddle; j+=1) {
+        pyramidHollow += '';
+    } if(q === w) {
+        pyramidHollow += '*';
+    } else {
+        pyramidHollow += '*';
+        for (let j=0; j<w; j+=1) {
             pyramidHollow += '';
         }
         pyramidHollow += '*';
@@ -84,18 +83,18 @@ console.log(pyramidHollow)
 // 6- Faça um programa que diz se um número definido numa variável é primo ou não.
 let number = 4294967297;
 let divisors = 0;
-for(i = 2; i<=number/2; i+=1) {
-    if (number % i === 0) {
-         divisors += 1;
-         break;
+for(let s = 2; s<=number; s+=1) {
+    if (number % s === 0) {
+        divisors += 1;
+        break;
     }
 }
-if (number == 1) {
+if (number === 1) {
     console.log('1 is neither prime nor composite.');
 } else {
     if (divisors === 0) 
     console.log(number + ' is a prime number.');
     else {
-        console.log(number + ' is not a prime number');
+        console.log(number + ' isn\'t a prime number.');
     }
 }
